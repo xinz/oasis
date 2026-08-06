7ecc824875f48824aa80a170a53f1542efc6506a
xinz <joy2everyone.z@gmail.com>
2026-08-06 10:44:33 +0800

    Update deps

--- mix.exs
+++ mix.exs
@@ -35,10 +35,10 @@ defmodule Oasis.MixProject do
       {:plug, "~> 1.11"},
       {:plug_crypto, "~> 1.2"},
       {:jason, "~> 1.2"},
-      {:jsonschex, "~> 0.9.0"},
+      {:jsonschex, "~> 0.9"},
       {:recase, "~> 0.7", runtime: false},
       {:plug_cowboy, "~> 2.0", only: [:dev, :test]},
-      {:cowboy, "~> 2.12.0", only: [:dev, :test]},
+      {:cowboy, "~> 2.18", only: [:dev, :test]},
       {:finch, "~> 0.6", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
